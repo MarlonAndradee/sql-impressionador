0d52b6ab124c3ef878b8da5816af418eb98546dc
Marlon Santos <marlon.andrade.santos@gmail.com>
2024-04-29 23:22:19 -0300

    Update and rename mod_07_exercicio_10.sql to modulo-07/mod_07_exercicio_10.sql

--- modulo-07/mod_07_exercicio_10.sql
+++ modulo-07/mod_07_exercicio_10.sql
@@ -1,6 +1,6 @@
-/*10. Faça uma tabela resumo mostrando o total de VacationHours para cada cargo (Title). Você
+/*10. FaÃ§a uma tabela resumo mostrando o total de VacationHours para cada cargo (Title). VocÃª
 deve considerar apenas as mulheres, dos departamentos de Production, Marketing,
-Engineering e Finance, para os funcionários contratados entre os anos de 1999 e 2000.*/
+Engineering e Finance, para os funcionÃ¡rios contratados entre os anos de 1999 e 2000.*/
 
 SELECT DISTINCT DepartmentName FROM DimEmployee
 ORDER BY DepartmentName ASC
