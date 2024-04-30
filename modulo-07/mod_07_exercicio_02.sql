1d65863eb25a956aa81e3baa3e51027af687ab12
Marlon Santos <marlon.andrade.santos@gmail.com>
2024-04-29 23:21:09 -0300

    Update and rename mod_07_exercicio_02.sql to modulo-07/mod_07_exercicio_02.sql

--- modulo-07/mod_07_exercicio_02.sql
+++ modulo-07/mod_07_exercicio_02.sql
@@ -1,14 +1,14 @@
 /*	
 	FACTSALES
 
-	2. Você precisa fazer uma análise de vendas por produtos. O objetivo final é descobrir o valor
+	2. VocÃª precisa fazer uma anÃ¡lise de vendas por produtos. O objetivo final Ã© descobrir o valor
 	total vendido (SalesAmount) por produto (ProductKey).
 
-	a) A tabela final deverá estar ordenada de acordo com a quantidade vendida e, além disso,
+	a) A tabela final deverÃ¡ estar ordenada de acordo com a quantidade vendida e, alÃ©m disso,
 	mostrar apenas os produtos que tiveram um resultado final de vendas maior do que
 	$5.000.000.
 
-	b) Faça uma adaptação no exercício anterior e mostre os Top 10 produtos com mais vendas.
+	b) FaÃ§a uma adaptaÃ§Ã£o no exercÃ­cio anterior e mostre os Top 10 produtos com mais vendas.
 	Desconsidere o filtro de $5.000.000 aplicado.  */
 
 	SELECT TOP(1000) * FROM FactSales
